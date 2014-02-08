f9a80cc124df1d5c288340a99447c38ea44edc75
Mark Menard <mark@mjm.net>
2014-02-07 22:53:12 -0500

    wiki example page builder with dependency injection.

--- wiki_example/lib/page_builder.rb
+++ wiki_example/lib/page_builder.rb
@@ -1,6 +1,7 @@
 class PageBuilder
 
   attr_accessor :buffer, :page_data, :include_suite_setup, :wiki_page
+  attr_writer :page_crawler
 
   def self.build_pseudo_markup (page_data, include_suite_setup)
     self.new(page_data, include_suite_setup).build_pseudo_markup
@@ -30,19 +31,19 @@ class PageBuilder
 
   def build_suite_setup
     return unless include_suite_setup
-    suite_setup = PageCrawlerImpl.inherited_page(SuiteResponder::SUITE_SETUP_NAME, wiki_page)
+    suite_setup = page_crawler.inherited_page(SuiteResponder::SUITE_SETUP_NAME, wiki_page)
     if suite_setup
       page_path = suite_setup.page_crawler.full_path(suite_setup)
-      page_path_name = PathParser.render(page_path)
+      page_path_name = path_parser.render(page_path)
       buffer.concat('!include -setup .').concat(page_path_name).concat("\n")
     end
   end
 
   def build_page_setup
-    setup = PageCrawlerImpl.inherited_page('SetUp', wiki_page)
+    setup = page_crawler.inherited_page('SetUp', wiki_page)
     if !setup.nil?
       setup_path = wiki_page.page_crawler.full_path(setup)
-      setup_path_name = PathParser.render(setup_path)
+      setup_path_name = path_parser.render(setup_path)
       buffer.concat('!include -setup .').concat(setup_path_name).concat("\n")
     end
   end
@@ -58,22 +59,30 @@ class PageBuilder
   end
 
   def build_page_teardown
-    teardown = PageCrawlerImpl.inherited_page('TearDown', wiki_page)
+    teardown = page_crawler.inherited_page('TearDown', wiki_page)
     if !teardown.nil?
       tear_down_path = wiki_page.page_crawler.full_path(teardown)
-      tear_down_path_name = PathParser.render(tear_down_path)
+      tear_down_path_name = path_parser.render(tear_down_path)
       buffer.concat("\n").concat('!include -teardown .').concat(tear_down_path_name).concat("\n")
     end
   end
 
   def build_suite_teardown
     return unless include_suite_setup
-    suite_teardown = PageCrawlerImpl.inherited_page(SuiteResponder::SUITE_TEARDOWN_NAME, wiki_page)
+    suite_teardown = page_crawler.inherited_page(SuiteResponder::SUITE_TEARDOWN_NAME, wiki_page)
     if !suite_teardown.nil?
       page_path = suite_teardown.page_crawler.full_path(suite_teardown)
-      page_path_name = PathParser.render(page_path)
+      page_path_name = path_parser.render(page_path)
       buffer.concat("\n").concat('!include -teardown .').concat(page_path_name).concat("\n")
     end
+  end
+
+  def page_crawler
+    @page_crawler ||= PageCrawlerImpl
+  end
+
+  def path_parser
+    @path_parser ||= PathParser
   end
 
 end
